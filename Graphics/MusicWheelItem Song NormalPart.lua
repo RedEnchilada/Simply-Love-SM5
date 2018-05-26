6c667aa470d3e155fe4a6a3d82b05cd6dcabea2a
RedEnchilada <red@lyrawearspants.com>
2018-05-25 22:12:37 -0500

    Adjust music wheel items to fit skewed layout better

--- Graphics/MusicWheelItem Song NormalPart.lua
+++ Graphics/MusicWheelItem Song NormalPart.lua
@@ -2,13 +2,19 @@ return Def.ActorFrame{
 	InitCommand=cmd(x, 26),
 
 	Def.Quad{
-		InitCommand=function(self) self:diffuse(color("#000a11")):zoomto(_screen.w/2.1675, _screen.h/15):diffusealpha(0.5) end
+		InitCommand=function(self)
+			self:diffuse(color("#000a11"))
+				:zoomto(_screen.w/2.1675, _screen.h/15)
+				:diffusealpha(0.5)
+				:skewx(_screen.h/_screen.w/12)
+		end
 	},
 	Def.Quad{
 		InitCommand=function(self)
 			self:diffuse( ThemePrefs.Get("RainbowMode") and Color.White or color("#0a141b"))
 				:diffusealpha( ThemePrefs.Get("RainbowMode") and 0.5 or 1 )
 				:zoomto(_screen.w/2.1675, _screen.h/15 - 1)
+				:skewx(_screen.h/_screen.w/12)
 		 end
 	}
 }
